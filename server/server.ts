f4e2c5be844caaee07eb6029b5203f9c214b26d9
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-07-10 16:15:31 +0300

    add connect & disconnect event listeners

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,21 @@
 import path from "path";
+import http from "http";
 import express from "express";
+import { Server as SocketIOServer } from "socket.io";
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Server is up on http://localhost:${port}`));
+io.on("connection", (socket) => {
+  console.log("A new user just connected");
+  socket.on("disconnect", () => console.log("User was disconnected"));
+});
+
+server.listen(port, () =>
+  console.log(`Server is up on http://localhost:${port}`),
+);
